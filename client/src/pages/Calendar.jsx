79cb3e70b5b6152f08a62e32f0be525d761db198
Edwu29 <edwardwu10@gmail.com>
2022-02-23 23:58:54 +0700

    basic calendar
    Co-authored-by: Evan Servito <eservito@uci.edu>

---
+++ client/src/pages/Calendar.jsx
@@ -0,0 +1,13 @@
+import './Calendar.css';
+
+import Calendar from 'react-calendar'
+
+function CalendarPage() {
+  return (
+    <div>
+    <Calendar/>
+    </div>
+  );
+}
+
+export default CalendarPage;
