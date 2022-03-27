d2016bb915967b1253e5ffa69a9444c0f6760ebc
Alan Chang <alan@alanchang.xyz>
2022-03-27 02:54:01 -0700

    Separate frontend/backend dependencies, install mongoose on backend, and initialize backend base with starting data schemas

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 * Frontend code resides in `client`
 * Backend code resides in `server`
-* Setup instructions are located in the `README.md` files in each folder 
+* Setup instructions are located in the `README.md` files in each folder.
+- Don't install dependencies to the base directory! Frontend dependencies should be separately installed into the `client` folder and backend dependencies should be separately installed into the `server` folder
--- server/app.js
+++ server/app.js
@@ -1,17 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
 
 require('dotenv').config();
 
-const app = express();
+// Connecting via mongoose
+const mongoURI = process.env.MONGO_URI;
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
+const mongoConnection = mongoose.connection;
+mongoConnection.once('open', () => {
+  console.log('MongoDB database connection established successfully');
+});
+
+// ExpressJS facilitates the backend
+const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(
-  cors({
-    origin: `${process.env.REACT_APP_HOST}:${process.env.REACT_APP_PORT}`,
-  }),
-);
+// CORS is needed so that our frontend can connect to our backend smoothly
+app.use(cors({ origin: `${process.env.REACT_APP_HOST}:${process.env.REACT_APP_PORT}`}));
+// Express.json() allows a request body to be passed into our backend routes
+app.use(express.json());
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
