abd5c1c5e7451941e6a5a537fa5fbfa0e1370789
Alan Chang <alan@alanchang.xyz>
2022-03-28 01:16:47 -0700

    Add first set of routes for database users and one GET route for appointments

--- server/routes/appointments.js
+++ server/routes/appointments.js
@@ -1,5 +1,19 @@
 const { Router } = require('express');
+const Appointment = require('../models/appointment.model');
 
 const router = Router();
+
+router.get('/', (req, res) => {
+  Appointment.find({}, (err, appts) => {
+    if (err) {
+      // console.log(err);
+      res.status(400).json({ error: 'Error fetching users' });
+    } else {
+      // console.log('Appointments: ');
+      // console.log(appts);
+      res.json(appts);
+    }
+  });
+});
 
 module.exports = router;
