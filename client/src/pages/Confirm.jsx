e6a5cf81bd939c87044e69b9bbb61ccfe98782d5
Hassan <hhalnajj@uci.edu>
2022-05-24 22:15:34 -0700

    confirm page styling. can't get button adjusted correctly

--- client/src/pages/Confirm.jsx
+++ client/src/pages/Confirm.jsx
@@ -6,24 +6,33 @@ function Confirm() {
   const navigate = useNavigate();
   const times = useLocation();
   return (
-    <div className="temp-change-later">
-      Success! Your appointment has been scheduled for:
-      <div className="date">
-        Date: {times.state.date[0]}/{times.state.date[1]}/{times.state.date[2]}
+    <div className="confirmBG">
+      <div className="confirmContainer">
+        <div className="head">
+          <h1>Success!</h1>
+        </div>
+        <div className="body">
+          Your appointment has been scheduled for:
+          <div className="date">
+            Date: {times.state.date[0]}/{times.state.date[1]}/{times.state.date[2]}
+          </div>
+          <div className="times">
+            {times.state.startTime} - {times.state.endTime}
+          </div>
+        </div>
+        <div className="foot">
+          <button
+            type="button"
+            onClick={() => {
+              navigate('/');
+            }}
+          >
+            RETURN TO CALENDAR
+          </button>
+        </div>
       </div>
-      <div className="times">
-        {times.state.startTime} - {times.state.endTime}
-      </div>
-      <button
-        type="button"
-        onClick={() => {
-          navigate('/');
-        }}
-      >
-        RETURN TO CALENDAR
-      </button>
     </div>
-  );
+    );
 }
 
 export default Confirm;
